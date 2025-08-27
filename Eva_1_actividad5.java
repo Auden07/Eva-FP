34a94d12e1a2edc65f35b533000fda3f814abfa1
Auden07 <L25550710@chihuahua2.tecnm.mx>
2025-08-26 23:02:32 -0600

    Add files via upload

---
+++ Eva_1_actividad5.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_1_actividad.pkg5;
+
+/**
+ *
+ * @author WinUser
+ */
+public class Eva_1_actividad5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("empleo:");
+        System.out.println("ingeniero en sistmas:");
+        System.out.println("salario:");
+        System.out.println("11000:");
+        System.out.println("horas semanales:");
+        System.out.println("65");
+        System.out.println("requiere ingles:");
+        System.out.println("si:");
+        
+        
+    }
+    
+}
