e9ffcbaaa423274cb4ff7d655dc9cd7662cc7ca0
Auden07 <L25550710@chihuahua2.tecnm.mx>
2025-08-26 22:43:36 -0600

    Add files via upload

---
+++ Eva1_3_PRINTR.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_3_printr;
+
+/**
+ *
+ * @author WinUser
+ */
+public class Eva1_3_PRINTR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Nombre del estudiante:");
+        System.out.println("AUDEN ESCARCEGA: ");
+        System.out.println("numero del contro:");
+        System.out.println("25550710:");
+        System.out.println("edad:");
+        System.out.println("18:");
+    }
+    
+}
---
+++ JavaApplication4.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication4;
+
+/**
+ *
+ * @author WinUser
+ */
+public class JavaApplication4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // //-- COMENNTARIO DE UNA LINEA 
+        //-- FIN DE ISTRUCCION
+        System.out.println("RUBEN HERNANDEZ");//CADENA DE TEXTO
+        System.out.println(1000);//numero entero
+        System.out.println(3.1416);//numero decimal
+        System.out.println(true);//valores logicos VEEDADERO O FALSO
+        System.out.println(´A´);//caracter 
+        System.out.println(´A´);//CADENA DE TEXTO
+    }
+    
+}
