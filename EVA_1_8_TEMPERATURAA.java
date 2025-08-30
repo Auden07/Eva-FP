b0ffe173b351ebd150f4684dbda7219ee4b6c874
Auden07 <L25550710@chihuahua2.tecnm.mx>
2025-08-29 18:01:58 -0600

    Add files via upload

---
+++ EVA_1_8_TEMPERATURAA.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_1_8_temperatura;
+
+/**
+ *
+ * @author WinUser
+ */
+public class EVA_1_8_TEMPERATURA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double C , F;
+        
+        C = 300;
+        F = C * 1.8 + 32;
+        
+        System.out.println("Para convertir de 300 * 1.8 + 32 a F");
+        System.out.println(F);
+        
+        
+    }
+    
+}
